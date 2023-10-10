33591ae2564fed9a8f49bbd211cd62fd258c4e90
Muhammed Yılmaz <mylmz2101@gmail.com>
2023-10-10 12:10:47 +0300

    file upload and download added

---
+++ Presentation/Controllers/FilesController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/files")]
+    public class FilesController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            //folder
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Media");
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            //path
+            var path = Path.Combine(folder, file.FileName);
+
+            //stream
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            //reponse body
+            return Ok(new
+            {
+                file = file.FileName,
+                path = path,
+                size = file.Length
+            });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Download(string fileName)
+        {
+            //file path
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Media", fileName);
+
+            //content type (MIME)
+            var provider = new FileExtensionContentTypeProvider();
+
+            if (!provider.TryGetContentType(fileName, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            //read
+            var bytes = await System.IO.File.ReadAllBytesAsync(filePath);
+
+            return File(bytes, contentType, Path.GetFileName(filePath));
+        }
+    }
+}
